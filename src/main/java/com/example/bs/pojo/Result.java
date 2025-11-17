ecb1e0e639f90ee57017ac8f6437276934e1d3f0
jiaran <2697442987@qq.com>
2025-11-17 19:41:23 +0800

    Init

---
+++ src/main/java/com/example/bs/pojo/Result.java
@@ -0,0 +1,28 @@
+package com.example.bs.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//接口响应类
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+    private String code;
+    private String message;
+    private Object data;
+
+    //    增删改响应
+    public static Result success(){
+        return new Result("1","success",null);
+    }
+    //    查询响应
+    public static Result success(Object data){
+        return new Result("1","success",data);
+    }
+    //    删除响应
+    public static Result error(String message){
+        return new Result("0",message,null);
+    }
+}
